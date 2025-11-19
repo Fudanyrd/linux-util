fb238cc162a89eb064488b7c71fb52b27f97d047
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2025-11-19 21:00:55 +0800

    [X] remove unused code

--- apt/download.cpp
+++ apt/download.cpp
@@ -47,40 +47,6 @@ int main(int argc, char **argv) {
 
   struct sockaddr_in saddr;
   saddr.sin_family = AF_INET;
-#if 0
-  /* HTTPS won't work. DELETE ME */
-  saddr.sin_port = htons(443);
-  for (const auto &addr : addrs) {
-    const unsigned char *b = (const unsigned char *)addr.addr_;
-    fprintf(stderr, "Trying %d.%d.%d.%d\n", b[0], b[1], b[2], b[3]);
-    *(uint32_t *)&saddr.sin_addr = *(uint32_t *)addr.addr_;
-    int rfd = open_clientfd((const sockaddr *)&saddr);
-    if (rfd < 0) {
-      return 1;
-    }
-    defer(close(rfd));
-
-    SSL *ssl = SSL_new(ctx);
-    if (!SSL_set_fd(ssl, rfd)) {
-      dbg.log("\tSSL_set_fd failed\n");
-      continue;
-    }
-
-    SSLSockFd ssfd;
-    ssfd.ssl_ = ssl;
-
-    HttpsClient client;
-    if (SSL_connect(ssl) != 1) {
-      dbg.log("\tSSL_connect failed\n");
-      continue;
-    }
-
-    std::vector<unsigned char> dummy;
-    auto ret =
-        client.request(ssfd, "GET", domain, argv[1], dummy, true, cookies);
-    (void)write(1, ret.data(), ret.size());
-  }
-#endif
   saddr.sin_port = htons(80);
   HttpClient client;
   for (const auto &addr : addrs) {
